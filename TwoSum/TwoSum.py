28114be69be4fa4fe02217f809448250e568a013
James Lee <james.soohyun@gmail.com>
2020-10-22 09:32:40 -0700

    TwoSum Bubble Sort solution

---
+++ TwoSum/TwoSum.py
@@ -0,0 +1,7 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, n):
+            if nums[i]+nums[j]==target:
+                return [i,j]
